8e122c485affd8f7d50418686e5c4eb8eb78169b
kellencataldo <kellencataldo@gmail.com>
2019-10-06 19:41:55 -0400

    Better command parser now

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,13 +1,8 @@
-#include "CommandParser.h"
+#include "ArgumentParser.h"
 #include <iostream>
 
 
 int main(int argc, char* args[]) {
-    commandParser parser(argc, args);
-    parser.addOption<int>("--cole", "retard", 5);
-    parser.addOption<int>("--colee", "retard", 12);
-    parser.addOptionList<int>("--colerrr", "asshole", 10, 20);
-    parser.parse();
 
-    return 0;
+   return 0;
 }
---
+++ include/ArgumentParser.h
@@ -0,0 +1,236 @@
+#pragma once
+
+#include <vector>
+#include <map>
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <iostream>
+#include <limits>
+
+#include "LogFormat.h"
+
+
+// @todo: comment and test this please
+
+class argumentParser {
+    public:
+        explicit argumentParser(const int argc, const char * const argv[]) :
+            m_arguments(argv + 1, argv + argc) { }
+
+        template <typename T>
+        void addCommand(const std::string command, const std::string helpText, const T defaultValue) {
+            validateThenAdd<T>(command, defaultValue, COMMAND_TYPE_POSITIONAL, helpText);
+        }
+
+        template <typename T>
+        T getCommand(const std::string command) {
+            return getValue<T>(command, COMMAND_TYPE_POSITIONAL);
+        }
+
+        template <typename T>
+        void addCommandList(const std::string command, const std::string helpText) {
+            validateThenAdd<std::vector<T>>(command, std::vector<T>(), COMMAND_TYPE_LIST, helpText);
+        }
+
+        template <typename T>
+        std::vector<T> getCommandList(const std::string command) {
+            return getValue<std::vector<T>>(command, COMMAND_TYPE_LIST);
+        }
+
+        void addCommandFlag(const std::string command, const std::string helpText) {
+            validateThenAdd<bool>(command, false, COMMAND_TYPE_FLAG, helpText);
+        }
+
+        bool getCommandFlag(const std::string command) {
+            return getValue<bool>(command, COMMAND_TYPE_FLAG);
+        }
+
+        template <typename T>
+        T getValue(std::string command) const {
+            if(m_commands.find(command) == m_commands.end()) {
+                std::string message("Unknown command: " + command);
+                throw std::invalid_argument(formatMessage(__FILE__, __func__, __LINE__, message));
+            }
+            return std::dynamic_pointer_cast<commandArgument<T>>(m_commands.find(command)->second)->m_value;
+        }
+
+        bool parse(void) {
+            if(m_arguments.empty() || m_arguments.at(0) == "--usage") {
+                displayHelpText();
+                return false;
+            }
+
+            std::shared_ptr<commandBase> sliding = nullptr;
+            for (auto argument = m_arguments.begin(); argument != m_arguments.end()-1; argument++) {
+                if(isCommand(*argument) && m_commands.find(*argument) != m_commands.end()) {
+                    if(sliding != nullptr && sliding->m_commandType == COMMAND_TYPE_POSITIONAL) {
+                        std::cout << "ERROR: ARGUMENTS REQUIRED FOR FLAG. SEE --usage" << std::endl;
+                        return false;
+                    }
+
+                    sliding = m_commands.find(*argument)->second;
+                    if(sliding->m_commandType == COMMAND_TYPE_FLAG) {
+                        std::dynamic_pointer_cast<commandArgument<bool>>(sliding)->m_value = true;
+                        sliding = nullptr;
+                    }
+                    continue;
+                }
+
+                else if(sliding != nullptr && sliding->parseArgument(*argument)) {
+                    sliding = (sliding->m_commandType == COMMAND_TYPE_POSITIONAL ? nullptr : sliding);
+                    continue;
+                }
+
+                std::cout << "MISMATCHED ARGUMENT SPECIFIED: " << *argument << ". SEE --usage" << std::endl;
+                return false;
+            }
+
+            if(sliding && sliding->m_commandType == COMMAND_TYPE_POSITIONAL) {
+                std::cout << "ERROR: ARGUMENTS REQUIRED FOR FLAG. SEE --usage" << std::endl;
+                return false;
+            }
+
+            m_queryString = *m_arguments.rbegin();
+            return true;
+        }
+
+    private:
+        enum COMMAND_TYPE {
+            COMMAND_TYPE_POSITIONAL,
+            COMMAND_TYPE_LIST,
+            COMMAND_TYPE_FLAG
+        };
+
+        class commandBase {
+            public:
+                explicit commandBase(const COMMAND_TYPE _commandType, const std::string _helpText) :
+                    m_commandType(_commandType), m_helpText(_helpText) { }
+                virtual ~commandBase(void) { }
+                virtual bool parseArgument(const std::string argument) = 0;
+                virtual std::string getArgString(void) const = 0;
+
+                COMMAND_TYPE m_commandType;
+                std::string m_helpText;
+        };
+
+        template <typename T>
+        class commandArgument : public commandBase {
+            public:
+                explicit commandArgument(const T _default, const COMMAND_TYPE _commandType,
+                        const std::string _helpText) :
+                    commandBase(_commandType, _helpText), m_value(_default) { }
+
+                bool parseArgument(const std::string argument) {
+                    try {
+                        argumentParser::convertArgument(argument, m_value);
+                    }
+                    catch(std::exception& e) {
+                        std::cout << e.what() << std::endl;
+                        return false;
+                    }
+                    return true;
+                }
+
+                std::string getArgString(void) const {
+                    return argumentParser::getRepresentation(m_value);
+                }
+
+                T m_value;
+        };
+
+        static bool isCommand(const std::string arg) {
+            if(arg.size() > 2 && arg[0] == '-' && arg[1] == '-' && std::all_of(arg.begin()+2, arg.end(),
+               [](const auto c){return std::isalpha(c);})) {
+                return true;
+            }
+            return false;
+        }
+
+        template<typename T>
+        void validateThenAdd(const std::string command, const T defaultVal, const COMMAND_TYPE commandType,
+                const std::string helpText) {
+            if(!isCommand(command)) {
+                std::string message("Option format is two hyphens followed by alphabetical characters");
+                throw std::invalid_argument(formatMessage(__FILE__, __func__, __LINE__, message));
+            }
+
+            if(m_commands.find(command) != m_commands.end()) {
+                std::string message("Duplicate option detected");
+                throw std::invalid_argument(formatMessage(__FILE__, __func__, __LINE__, message));
+            }
+
+            m_commands.emplace(command, std::make_shared<commandArgument<T>>(defaultVal, commandType, helpText));
+        }
+
+
+        void displayHelpText(void) const {
+            size_t maxLength = std::max_element(m_commands.begin(), m_commands.end(),
+                    [](const auto& lhs, const auto& rhs)
+                    {return lhs.first.size() < rhs.first.size();})->first.size() + 50;
+                std::cout << "WELCOME TO PORT QUERY. WHY ARE YOU USING THIS." << std::endl;
+                std::cout << "USAGE: [OPTION1, OPTION2...] QUERY_STRING" << std::endl;
+                std::cout.width(maxLength);
+                std::cout << std::left << "    OPTION" << "HELP" << std::endl;
+                std::for_each(m_commands.begin(), m_commands.end(), [&maxLength](const auto& e){
+                    std::cout.width(maxLength);
+                    std::cout  << ("    " + e.first + " " + e.second->getArgString());
+                    std::cout << e.second->m_helpText << std::endl;});
+        }
+
+        static void convertArgument(std::string argument, int& value) {
+            value = std::stoi(argument);
+        }
+
+        static void convertArgument(std::string argument, std::string& value) {
+            value = argument;
+        }
+
+        static void convertArgument(std::string argument, bool& value) {
+            std::string message("Boolean commands do not get converted");
+            throw std::invalid_argument(formatMessage(__FILE__, __func__, __LINE__, message));
+        }
+
+        template<typename T>
+        static void convertArgument(std::string argument, std::vector<T>& valueVector) {
+            T value;
+            argumentParser::convertArgument(argument, value);
+            valueVector.push_back(value);
+        }
+
+        static std::string getRepresentation(const std::string value) {
+            return "[string]";
+        }
+
+        static std::string getRepresentation(const int value) {
+            return "[int]";
+        }
+
+        static std::string getRepresentation(const bool value) {
+            return "";
+        }
+
+        template<typename T>
+        static std::string getRepresentation(const std::vector<T> value) {
+            T baseValue;
+            return "{" + argumentParser::getRepresentation(baseValue) + "...}";
+        }
+
+        template <typename T>
+        T getValue(const std::string command, const COMMAND_TYPE commandType) const {
+            const auto commandCandidate = m_commands.find(command);
+            if(commandCandidate == m_commands.end()) {
+                std::string message("Unknown command: " + command);
+                throw std::invalid_argument(formatMessage(__FILE__, __func__, __LINE__, message));
+            } else if (commandCandidate->second->m_commandType != commandType) {
+                std::string message("Requested command type does not match actual command type");
+                throw std::invalid_argument(formatMessage(__FILE__, __func__, __LINE__, message));
+            }
+
+            return std::dynamic_pointer_cast<commandArgument<T>>(commandCandidate->second)->m_value;
+        }
+
+        std::vector<std::string> m_arguments;
+        std::map<std::string, std::shared_ptr<commandBase>> m_commands;
+        std::string m_queryString;
+};
