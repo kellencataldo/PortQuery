0f25b1533ce5b1f12d2c2f5a4a4c43c6b2b70a97
kellencataldo <kellencataldo@gmail.com>
2020-01-04 20:09:38 -0500

    switched to g test, was easier than expected

--- tests/TestArgumentParser.cpp
+++ tests/TestArgumentParser.cpp
@@ -1,31 +1,32 @@
-#include "catch.hpp"
-#include "../cli/ArgumentParser.h"
+#include "gtest/gtest.h"
+#include "../cliportquery/ArgumentParser.h"
 #include <iostream>
 
 
-TEST_CASE("Help message found and counts as a fail", "[ArgumentParser]") {
+// Help message found and counts as a fail
+TEST(ParseHelpCommand, ReturnsFalse) {
     
     // No arguments supplied counts as a fail
     const int argc_T1 = 1;
     const char* args_T1[argc_T1] = {"./cli"};
-    REQUIRE(argumentParser(argc_T1, args_T1).parse() == false);
+    EXPECT_FALSE(argumentParser(argc_T1, args_T1).parse());
 
     // Usage flag alone counts as a fail
     const int argc_T2 = 2;
     const char * args_T2[argc_T2] = {"./cli", "--usage"};
-    REQUIRE(argumentParser(argc_T2, args_T2).parse() == false);
+    EXPECT_FALSE(argumentParser(argc_T2, args_T2).parse());
 
     // Usage flag specified anywhere in command counts as a fail
     const int argc_T3 = 4;
     const char * args_T3[argc_T3] = {"./cli", "--valid", "--usage", "potential_query_string"};
     argumentParser parser(argc_T3, args_T3);
     parser.addCommandFlag("--valid", "dummy command");
-    REQUIRE(parser.parse() == false);
+    EXPECT_FALSE(parser.parse());
 }
 
 
-TEST_CASE("Positional arguments can be correctly parsed", "[ArgumentParser] [Positional]") {
-
+// Positional arguments can be correctly parsed
+TEST(ParseArguments, PositionalArguments) {
     const int argc_T1 = 6;
     const char * args_T1[argc_T1] = {"./cli", "--debug", "5", "--term", "search", "query_string_here"};
     argumentParser parser(argc_T1, args_T1);
@@ -33,13 +34,13 @@ TEST_CASE("Positional arguments can be correctly parsed", "[ArgumentParser] [Pos
     parser.addCommand<std::string>("--term", "help text", std::string("default"));
     parser.addCommand<std::string>("--file", "help text", std::string("/some/file"));
 
-    REQUIRE(parser.parse() == true);
-    REQUIRE(5 == parser.getCommand<int>("--debug"));
-    REQUIRE("search" == parser.getCommand<std::string>("--term"));
-    REQUIRE("/some/file" == parser.getCommand<std::string>("--file"));
+    ASSERT_TRUE(parser.parse());
+    ASSERT_EQ(5, parser.getCommand<int>("--debug"));
+    EXPECT_STREQ("search", parser.getCommand<std::string>("--term").c_str());
+    EXPECT_STREQ("/some/file", parser.getCommand<std::string>("--file").c_str());
 
-    REQUIRE_THROWS(parser.getCommand<std::string>("--nocommand"));
-    REQUIRE("query_string_here" == parser.getQueryString());
+    EXPECT_THROW(parser.getCommand<std::string>("--nocommand"), std::invalid_argument);
+    EXPECT_STREQ("query_string_here", parser.getQueryString().c_str());
 
     // Command argument which cannot be converted counts as a fail
     const int argc_T2 = 4;
@@ -47,17 +48,18 @@ TEST_CASE("Positional arguments can be correctly parsed", "[ArgumentParser] [Pos
     argumentParser parser2(argc_T2, args_T2);
 
     parser2.addCommand<int>("--intcommand", "help text", 1);
-    REQUIRE(false == parser2.parse());
+    EXPECT_FALSE(parser2.parse());
 
     // Command with no argument supplied counts as a fail
     const int argc_T3 = 3;
     const char * args_T3[argc_T3] = {"./cli", "--command", "query_string_here"};
     argumentParser parser3(argc_T3, args_T3);
-    REQUIRE(false == parser3.parse());
+    EXPECT_FALSE(parser3.parse());
 }
 
 
-TEST_CASE("Flag arguments can be correctly parsed", "[ArgumentParser] [Flag]") {
+// flag arguments can be correctly parsed
+TEST(ParseArguments, FlagArguments) {
     const int argc_T1 = 4;
     const char * args_T1[argc_T1] = {"./cli", "--first", "--second", "potential_query_string"};
 
@@ -65,28 +67,29 @@ TEST_CASE("Flag arguments can be correctly parsed", "[ArgumentParser] [Flag]") {
     parser.addCommandFlag("--first", "help text");
     parser.addCommandFlag("--second", "help text");
     parser.addCommandFlag("--third", "help text");
-    REQUIRE(true == parser.parse());
-    REQUIRE(true == parser.getCommandFlag("--first"));
-    REQUIRE(true == parser.getCommandFlag("--second"));
-    REQUIRE(false == parser.getCommandFlag("--third"));
+    ASSERT_TRUE(parser.parse());
+    EXPECT_TRUE(parser.getCommandFlag("--first"));
+    EXPECT_TRUE(parser.getCommandFlag("--second"));
+    EXPECT_FALSE(parser.getCommandFlag("--third"));
 
-    REQUIRE_THROWS(parser.getCommandFlag("--noflag"));
-    REQUIRE("potential_query_string" == parser.getQueryString());
+    EXPECT_THROW(parser.getCommandFlag("--noflag"), std::invalid_argument);
+    EXPECT_STREQ("potential_query_string", parser.getQueryString().c_str());
 }
 
 
-TEST_CASE("List arguments can be correctly parsed" "[ArgumentParser] [List]") {
+// List arguments can be correctly parsed
+TEST(ParseArguments, ListArguments) {
 
     const int argc_T1 = 6;
     const char * args_T1[argc_T1] = {"./cli", "--list", "1", "2", "3", "query_string_here"};
     argumentParser parser(argc_T1, args_T1);
 
     parser.addCommandList<int>("--list", "help text");
-    REQUIRE(true == parser.parse());
+    ASSERT_TRUE(parser.parse());
     std::vector<int> intList = parser.getCommandList<int>("--list");
-    REQUIRE(intList.size() == 3);
-    REQUIRE((1 == intList.at(0) && 2 == intList.at(1) && 3 == intList.at(2))); 
-    REQUIRE("query_string_here" == parser.getQueryString());
+    ASSERT_EQ(3, intList.size());
+    EXPECT_TRUE((1 == intList.at(0) && 2 == intList.at(1) && 3 == intList.at(2))); 
+    EXPECT_STREQ("query_string_here", parser.getQueryString().c_str());
 
     // List population stops after another flag is found
     const int argc_T2 = 6;
@@ -95,13 +98,13 @@ TEST_CASE("List arguments can be correctly parsed" "[ArgumentParser] [List]") {
 
     parser2.addCommandList<std::string>("--list", "help text");
     parser2.addCommandFlag("--unrelated", "help text");
-    REQUIRE(true == parser2.parse());
+    ASSERT_TRUE(parser2.parse());
     
     std::vector<std::string> stringList = parser2.getCommandList<std::string>("--list");
-    REQUIRE(2 == stringList.size());
-    REQUIRE(("arg1" == stringList.at(0) && "arg2" == stringList.at(1)));
-    REQUIRE(true == parser2.getCommandFlag("--unrelated"));
-    REQUIRE("query_string_here" == parser2.getQueryString());
+    ASSERT_EQ(2, stringList.size());
+    EXPECT_TRUE(("arg1" == stringList.at(0) && "arg2" == stringList.at(1)));
+    EXPECT_TRUE(parser2.getCommandFlag("--unrelated"));
+    EXPECT_STREQ("query_string_here", parser2.getQueryString().c_str());
 }
 
 
