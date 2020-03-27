748b8924b42ae79066bb56117959fac9aa5fbb58
kellencataldo <kellencataldo@gmail.com>
2020-03-27 19:33:26 -0400

    small change

--- libportquery/source/Lexer.cpp
+++ libportquery/source/Lexer.cpp
@@ -5,6 +5,22 @@
 
 Token Lexer::nextToken() {
 
+    // This is an optimization for the scenario that the next token has been scanned and identified
+    // but the input hasn't been advanced. This occurs when a token is "peeked"
+
+    // First, check to see if we have already parsed the next token, just not advanced the input
+    if (std::nullopt != m_peekToken) {
+        // move the value that was in the peek token to the return token
+        Token outToken(std::move(m_peekToken.value()));
+        // Reset the peek token to an empty state, will be set again next time peek() is called
+        m_peekToken.reset();
+        // Return the token to the user
+        return outToken;
+    }
+
+    // The peek token was empty, parse the next token the usual way.
     return EOFToken{};
 
 }
+
+
